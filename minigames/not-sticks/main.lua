9dd58fc7e02570e3de362b56e64497998ee166a6
Jacob McKay <jacob_mckay@bradycorp.com>
2018-04-18 22:10:52 -0500

    Made the illusion of a fun 'avoid the enemy' game  - Grokking Lua syntax, Love2d api, Lua modules

--- minigames/not-sticks/main.lua
+++ minigames/not-sticks/main.lua
@@ -1,68 +1,97 @@
+flux = require("minigames/not-sticks/lib/flux")
+
+love.window.setMode(500, 500)
+
 local Game = {}
 
 function Game:load(arg)
     circle = {}
     circle.x = 150			-- This will render the circle at 150px to the right along the x-axis.
-    circle.y = 150			-- This will render the circle at 150px below along the y-axis.
+	circle.y = 150			-- This will render the circle at 150px below along the y-axis.
+	circle.radius = 25
     circle.speed = 500		-- This will give the circle a movement speed of 500.
  
     rect = {}
     rect.x = 300			-- This will render the rectangle at 300px to the right along the x-axis.
     rect.y = 150			-- This will render the rectangle at 150px below along the y-axis.
-    rect.w = 100			-- This will render the rectangle with a width of 100px.
-    rect.h = 100			-- This will render the rectangle with a height of 100px.
-    rect.speed = 100		-- This will give the rectangle a movement speed of 100.
+    rect.w = 50			-- This will render the rectangle with a width of 100px.
+    rect.h = 50			-- This will render the rectangle with a height of 100px.
+	rect.speed = 100		-- This will give the rectangle a movement speed of 100.
+
+	score = 0
+	timeElapsed = 0
+	nextAttackTime = 1
+	timeBetweenAttacks = 1
+	timeBetweenAttacksDecay = 0.02
+	attackDuration = 0.5
+	attackDurationDecay = 0.01
 end
  
 function Game:update(dt)
-    if love.keyboard.isDown("d") then 				-- If player is holding down 'D',
-	circle.x = circle.x + (circle.speed * dt)		-- Move circle to the right.
+    local shouldDoAttack = false
+    timeElapsed = timeElapsed + dt
+    -- print("time elapsed: " .. timeElapsed .. " next attack time: " .. nextAttackTime)
+    if timeElapsed > nextAttackTime then
+      nextAttackTime = nextAttackTime + timeBetweenAttacks
+      shouldDoAttack = true
+    end
+
+    if love.keyboard.isDown("d") then 
+		circle.x = circle.x + (circle.speed * dt)	
     end
  
-    if love.keyboard.isDown("a") then 				-- If player is holding down 'A',
-	circle.x = circle.x - (circle.speed * dt)		-- Move circle to the left.
+    if love.keyboard.isDown("a") then 		
+		circle.x = circle.x - (circle.speed * dt)		
     end
  
-    if love.keyboard.isDown("w") then 				-- If player is holding down 'W',
-	circle.y = circle.y - (circle.speed * dt)		-- Move circle upwards.
+    if love.keyboard.isDown("w") then 				
+		circle.y = circle.y - (circle.speed * dt)		
     end
  
-    if love.keyboard.isDown("s") then 				-- If player is holding down 'S',
-	circle.y = circle.y + (circle.speed * dt)		-- Move circle downwards.
+    if love.keyboard.isDown("s") then 
+		circle.y = circle.y + (circle.speed * dt)		
     end
- 
-    if rect.x < circle.x then 					-- If the rect is to the left of the circle:
-	rect.x = rect.x + (rect.speed * 2.5 * dt)		-- Rectangle moves towards the right.
-    end
- 
-    if rect.x > circle.x then 					-- If the rect is to the right of the circle:
-	rect.x = rect.x - (rect.speed * 2.5 * dt) 		-- Rectangle moves towards the left.
-    end
- 
-    if rect.y < circle.y then 					-- If the rect is above the circle:
-	rect.y = rect.y + (rect.speed * 2.5 * dt)		-- Rectangle moves downward.
-    end
- 
-    if rect.y > circle.y then 					-- If the rect is below the circle:
-	rect.y = rect.y - (rect.speed * 2.5 * dt)		-- Rectangle moves upward.
+
+	if shouldDoAttack then
+		shouldDoAttack = false
+		
+		attackCompletCallback = function()
+			if rect.x == (circle.x - (rect.w / 2)) and rect.y == (circle.y - (rect.h / 2)) then
+				self.load()
+				print("too slow, they got you!")
+			end
+			score = score + 1
+			print("SCORE: " .. score)
+			timeBetweenAttacks = timeBetweenAttacks - timeBetweenAttacksDecay
+        end
+
+        local attackTargetX, attackTargetY = (circle.x - (rect.w / 2)), (circle.y - (rect.h / 2)) 
+        flux.to(rect, attackDuration, { x = attackTargetX, y = attackTargetY }):ease("elasticin"):oncomplete(attackCompletCallback)
+        attackDuration = attackDuration - attackDurationDecay
     end
 
     if love.keyboard.isDown("escape") then
         print("be that way then")
         love.event.quit() 
     end
+
+    flux.update(dt)
 end
  
 function Game:draw()
-    love.graphics.setColor(255, 0, 0)
-    love.graphics.circle("fill", circle.x, circle.y, 50)
- 
     love.graphics.setColor(0, 255, 0)
-    love.graphics.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
+	love.graphics.circle("fill", circle.x, circle.y, circle.radius)
+	
+	love.graphics.print("Current Score: " .. score, x, y)
+
+    love.graphics.setColor(255, 0, 0)
+	love.graphics.rectangle("fill", rect.x, rect.y, rect.w, rect.h)
+	
 end
 
-function Game:contact(player, stick, coll)
-    print("collision!")
+function Game:contact(youHitMe, noYouHitME, collision)
+    -- TODO 'real physics' https://love2d.org/wiki/Tutorial:Physics
+    print("collision!") -- I don't know love physics yet
 end
 
 return Game
